595cc375cc2d326f2fd5ab7d48e9f84f65c4770c
Anderson Lucena <obeloanarquista@gmail.com>
2024-01-24 06:03:06 -0300

    Add files via upload

---
+++ solicitar_dois_números_do_usuario_e_somar.py
@@ -0,0 +1,9 @@
+# Solicitar dois números do usuário
+numero1 = float(input("Informe o primeiro número: "))
+numero2 = float(input("Informe o segundo número: "))
+
+# Calcular a soma
+soma = numero1 + numero2
+
+# Exibir a soma
+print(f"A soma dos números informados é: {soma}")
